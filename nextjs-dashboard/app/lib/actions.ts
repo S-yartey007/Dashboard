d6be67c05d4e897338fcc98fd276b953e12491f8
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-07-27 21:06:07 +0000

    Error handling

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -11,6 +11,34 @@ const FormSchema = z.object({
     date: z.string(),
 })
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+    
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+
+   try{
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+   } catch(err) {
+    return {
+        message: "Database Error: Failed to update Invoice"
+    }
+   }
+    
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
 const CreateInvoice = FormSchema.omit({id: true,date: true});
 
 export async function createInvoice(formData: FormData) {
@@ -24,12 +52,31 @@ export async function createInvoice(formData: FormData) {
     
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    } catch(error) {
+        return {
+            message : "Database Error: Failed to create Invoice"
+        }
+    }
+  
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 
 }
+
+export async function deleteInvoice(id: string) {
+    try{
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+    } catch(error) {
+        return {
+            message: "Database Error: Failed to delete Invoice"
+        }
+    }
+    revalidatePath('/dashboard/invoices');
+  }
